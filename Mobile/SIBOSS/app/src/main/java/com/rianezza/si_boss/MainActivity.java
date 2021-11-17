7537b7f0b29d02affe52ef9c265530b1a1d30e90
alfian_ezza <rianezza86@gmail.com>
2021-11-17 23:57:40 +0700

    Upload android
    Only base layout included (not all)

---
+++ Mobile/SIBOSS/app/src/main/java/com/rianezza/si_boss/MainActivity.java
@@ -0,0 +1,44 @@
+package com.rianezza.si_boss;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.DatePickerDialog;
+import android.os.Bundle;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity {
+
+    private DatePickerDialog datePickerDialog;
+    private SimpleDateFormat simpleDateFormat;
+    private TextView dateResult;
+    private EditText datePicker;
+
+    private void showDateDialog(){
+        Calendar newCalendar = Calendar.getInstance();
+        datePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+
+                Calendar newDate = Calendar.getInstance();
+                newDate.set(year, monthOfYear, dayOfMonth);
+
+                dateResult.setText(simpleDateFormat.format(newDate.getTime()));
+            }
+
+        },newCalendar.get(Calendar.YEAR), newCalendar.get(Calendar.MONTH), newCalendar.get(Calendar.DAY_OF_MONTH));
+
+        datePickerDialog.show();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ Mobile/SIBOSS/app/src/main/java/com/rianezza/si_boss/search_result.java
@@ -0,0 +1,14 @@
+package com.rianezza.si_boss;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class search_result extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.result_search);
+    }
+}
