a30190657da340d5b78cc6d24c2d8a40441d7f30
Rapli87 <raflialfajar1987@gmail.com>
2021-12-16 23:20:38 +0700

    update

--- Mobile/Login Register/SIBOSS/app/src/main/java/com/rafli/si_boss/SplashScreenActivity.java
+++ Mobile/Login Register/SIBOSS/app/src/main/java/com/rafli/si_boss/SplashScreenActivity.java
@@ -1,31 +1,54 @@
 package com.rafli.si_boss;
 
-import android.annotation.SuppressLint;
-import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
+import android.view.WindowManager;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-@SuppressLint("CustomSplashScreen")
+
+
+//@SuppressLint("CustomSplashScreen")
 public class SplashScreenActivity extends AppCompatActivity {
 
-    @Override
+    ProgressBar progressBar;
+    TextView textView;
+
+        @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.splashscreen);
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.splashscreen);
 
-//        inisialisai handler
-        Handler handler = new Handler();
+            getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                    WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
-//        Start handler 1 detik
-        handler.postDelayed(() -> {
-//          Insialisasi intent ke halaman login
-            Intent loginIntent = new Intent(getApplicationContext(), LoginActivity.class);
-                    startActivity(loginIntent);
+            progressBar = findViewById(R.id.progressBar);
+            textView = findViewById(R.id.text_view);
 
-            finish();
-        }, 3000l); // Menampilkan splashscreen selama 3 detik
-    }
+            progressBar.setMax(100);
+            progressBar.setScaleY(3f);
+
+            progresAnimation();
+        }
+
+        public void progresAnimation(){
+            ProgressBarAnimation anim = new ProgressBarAnimation(this, progressBar, textView, 0f, 100f);
+            anim.setDuration(10000);
+            progressBar.setAnimation(anim);
+        }
 }
+//
+////        inisialisai handler
+//        Handler handler = new Handler();
+//
+////        Start handler 1 detik
+//        handler.postDelayed(() -> {
+////          Insialisasi intent ke halaman login
+//            Intent loginIntent = new Intent(getApplicationContext(), LoginActivity.class);
+//                    startActivity(loginIntent);
+//
+//            finish();
+//        }, 3000l); // Menampilkan splashscreen selama 3 detik
+
 
